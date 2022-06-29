c046a25c5755d1c2d1641d8d787bf42b1bee2561
orviant20011904 <92343935+orviant20011904@users.noreply.github.com>
2022-06-29 15:40:58 -0500

    Librerias Adicionales

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 const express = require('express')
 const path = require('path')
-const route = require('./routes/index')
+const index = require('./routes/index')
+const login=require('./routes/login')
+const date=require('./routes/appointment')
 
+//Connection to the databaseName
+
+require('./drivers/drivers-data')
 //Inicializations
 const app = express()
 app.use(express.static(path.join(__dirname,'public')))
@@ -11,11 +16,14 @@ app.use(express.static(path.join(__dirname,'public')))
 app.set('port', process.env.PORT || 8080)
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
 
 //Middlewares
 
 //Routes
-app.use('/',route)
+app.use('/',index)
+app.use('/login',login)
+app.use('/appointment',date)
 
 //Start server
 app.listen(app.get('port'), () => {
