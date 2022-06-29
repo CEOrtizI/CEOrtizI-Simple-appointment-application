0fce5dd38aedebee16db19e32fa0ad771782be1f
Alicia Porras <39271391+aliciaPorras@users.noreply.github.com>
2022-06-29 16:48:49 -0500

    cambios en appoinment

--- models/appoinment.js
+++ models/appoinment.js
@@ -2,9 +2,10 @@ var mongoose =require('mongoose');
 schema =mongoose.schema;
 var appointment= new schema({
     appID: 	{type:String},
-    appID:	{type:String},
-    patientID: 	{type:String},
     date:	{type:Date},
-    time:	{type:Date}
+    time:	{type:Date},
+    doctorID:	{type:Schema.Types.ObjectId, ref:'Doctor'},
+    patientID: 	{type:Schema.Types.ObjectId, ref:'patient'},
+  
 })
 module.exports=mongoose.model('Doctor',Doctor)
